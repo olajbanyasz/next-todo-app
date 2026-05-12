bae9150ad7e4cbedd2fadd0c31d6a90ce2179a00
Zsolt Andre <zsolt_andre@epam.com>
2026-05-12 16:42:29 +0200

    navbar extended with github avatar

--- lib/auth.ts
+++ lib/auth.ts
@@ -57,7 +57,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           id: user.id,
           email: user.email,
           name: user.name,
-          role: user.role
+          role: user.role,
+          image: user.image
         }
       }
     })
